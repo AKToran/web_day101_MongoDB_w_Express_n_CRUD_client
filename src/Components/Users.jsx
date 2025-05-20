7c05db785fac6553f934702061e48de0309b7f0a
AKToran <aktoran60@gmail.com>
2025-05-20 22:24:30 +0600

    load specific user client

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,19 +1,16 @@
 import React, { use, useState } from "react";
+import { Link } from "react-router";
 
 const Users = ({ usersPromise }) => {
   const initialUsers = use(usersPromise);
-
   const [users, setUsers] = useState(initialUsers);
 
   const handleAddUser = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
-
     const newUser = { name, email };
-
     // console.log(newUser);
-
     //create user in db:
     fetch("http://localhost:3000/users", {
       method: "POST",
@@ -68,6 +65,7 @@ const Users = ({ usersPromise }) => {
         {users.map((user) => (
           <p key={user._id}>
             {user.name} : {user.email}{" "}
+            <Link to={`/users/${user._id}`}>Details</Link>
             <button onClick={() => handleUserDeletion(user._id)}>X</button>{" "}
           </p>
         ))}
